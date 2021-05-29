da568f4193755534febac6fcf89a4e9c1e329e48
sun-23 <twinsamson@gmail.com>
2021-05-29 12:51:16 +0700

    add stl viewer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import "./styles.css";
-import React, { useState } from "react";
+import { useState } from "react";
+import STLViewer from './STLViewer';
 import NodeStl from "./stl" ;
 var Buffer = require("buffer/").Buffer;
 
-export default function App() {
+function App() {
   const [stl_file, setFile] = useState();
   const [stl_cal, setSTL_Cal] = useState();
-  const [material, setMaterial] = useState(1.38);
+  const [material, setMaterial] = useState(1.27);
+  const [infill, setInfill] = useState(20);
+  const [price, setPrice] = useState(0);
   const [message, setMessage] = useState('please click submit button');
+
+  const selectFile = (e) => {
+    var file = e.target.files[0];
+    if(file.name.toLocaleLowerCase().endsWith('.stl')){
+      setFile(file);
+    } else {
+      alert(' Invalid file type. Only STL files are supported. Please select a new file. ');
+    }
+  }
 
   const submitStl = () => {
     setSTL_Cal(null);
@@ -22,23 +34,26 @@ export default function App() {
       var stl = new NodeStl(buffer, {density: material});
       setSTL_Cal(stl)
       console.log(stl);
+      calculatingPrice(stl);
     });
+  }
+
+  const calculatingPrice  = (stl_cal) => {
+    setPrice((50 + stl_cal.weight * (infill / 100) * 10) < 100 ? 100 : (50 + stl_cal.weight * (infill / 100) * 10));
   }
 
   const changeMaterial = (e) => {
     console.log('click', e.target.value);
     setMaterial(parseFloat(e.target.value));
     setSTL_Cal(null);
+    setMessage('please click submit button');
   }
 
-  const selectFile = (e) => {
-    var file = e.target.files[0];
-    console.log(file.name.toLocaleLowerCase());
-    if(file.name.toLocaleLowerCase().endsWith('.stl')){
-      setFile(file);
-    } else {
-      alert(' Invalid file type. Only STL files are supported. Please select a new file. ');
-    }
+  const changeInfill = (e) => {
+    console.log('click', e.target.value);
+    setInfill(parseFloat(e.target.value));
+    setSTL_Cal(null);
+    setMessage('please click submit button');
   }
 
   return (
@@ -51,20 +66,40 @@ export default function App() {
         onChange={(e) => selectFile(e)}
       ></input>
       <input type="submit" onClick={submitStl}></input>
-        <label for="cars">  Material: </label>
-        <select name="Material" onChange={(e) => changeMaterial(e)}>
-          <option value="1.38">PETG</option>
-          <option value="1.24">PLA</option>
-        </select>
-        <p>Material density: {material} g/cm^3</p>
+      <label for="material">  Material: </label>
+      <select name="Material" onChange={(e) => changeMaterial(e)}>
+        <option value="1.27">PETG</option>
+        <option value="1.24">PLA</option>
+      </select>
+      <label for="infill">  Infill: </label>
+      <select name="Infill" onChange={(e) => changeInfill(e)}>
+        <option value="20">20 %</option>
+        <option value="40">40 %</option>
+        <option value="60">60 %</option>
+        <option value="80">80 %</option>
+        <option value="100">100 %</option>
+      </select>
+      <p>Material density: {material} g/cm^3</p>
       {stl_cal ? <div>
-        <h2>Calculator</h2>
+        <h2>Result</h2>
+        <h3>price: {price}</h3>
         <h3>volume: {stl_cal.volume} cm^3</h3>
         <h3>boundingBox: {stl_cal.boundingBox[0]} {stl_cal.boundingBox[1]} {stl_cal.boundingBox[2]} mm</h3>
-        <h3>weight: {stl_cal.weight} gm</h3>
+        {/* <h3>weight not include infill: {stl_cal.weight} gm</h3> 
         <h3>area: {stl_cal.area} m^2</h3>
-        <h3>center of mass: {stl_cal.centerOfMass[0]} {stl_cal.centerOfMass[1]} {stl_cal.centerOfMass[2]} mm</h3>
+        <h3>center of mass: {stl_cal.centerOfMass[0]} {stl_cal.centerOfMass[1]} {stl_cal.centerOfMass[2]} mm</h3> */}
       </div> : <p>{message}</p>}
+      {stl_file ? <STLViewer
+        onSceneRendered={(element) => {
+            console.log(element)
+        }}
+        sceneClassName="test-scene"
+        file={stl_file}
+        className="obj"
+        modelColor="#FF0000"/>
+      : null }
     </div>
   );
 }
+
+export default App;
