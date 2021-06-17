53f99699a4ef0617be36fc64caf75cc687bcac26
sun-23 <twinsamson@gmail.com>
2021-06-17 20:26:28 +0700

    change m to n

--- src/Component/Select_Quality.js
+++ src/Component/Select_Quality.js
@@ -6,7 +6,7 @@ export default function Select_Quality({ onChangeQuality }) {
             </div>
             <div className="col-10">
                 <select class="form-select" name="" onChange={(e) => onChangeQuality(e)}>
-                    <option value="1">Meduim (0.2mm)</option>
+                    <option value="1">Normal (0.2mm)</option>
                     <option value="1.1">High (0.15mm)</option>
                     <option value="0.9">Low (0.3mm)</option>
                 </select>
